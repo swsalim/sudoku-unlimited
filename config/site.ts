0a0908cffbc94657e26fb51a854b3fae32094ec6
Yuyu S <swsalim@gmail.com>
2025-03-15 23:22:18 +0800

    fix site name

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ import { absoluteUrl } from '@/lib/utils';
 export const siteConfig: SiteConfig = {
   title: 'Play Free Sudoku Unlimited online - solve web sudoku puzzles',
   description:
-    'Play free Sudoku online from Easy to Expert level on Sudoku.com. Select a difficulty level of a web sudoku puzzle to challenge yourself and enjoy the game!',
+    'Play free Sudoku online from Easy to Expert level on SudokuUnlimited.com. Select a difficulty level of a web sudoku puzzle to challenge yourself and enjoy the game!',
   siteName: 'Sudoku Unlimited',
   url: new URL(absoluteUrl()),
   openGraph: {
